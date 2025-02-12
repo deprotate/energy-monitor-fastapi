af543e97ccee295c693cfa3c1c3a4f3695447f66
deprotate <gribozui.top@gmail.com>
2025-02-13 01:40:58 +0300

    aaaa

--- api_v1/core/config.py
+++ api_v1/core/config.py
@@ -13,10 +13,12 @@ class Settings(BaseSettings):
 
     @property
     def db_url(self):
-        url = os.getenv("DATABASE_URL")
-        url = "postgresql+asyncpg"+ url[9::]
+        url = os.getenv("DATABASE_URL", "")
+        if url.startswith("postgres://"):
+            url = url.replace("postgres://", "postgresql+asyncpg://", 1)
         return url
-       # return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database}"
+
+    # return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database}"
     #db_url: str = "postgresql+asyncpg://postgres:utehLuQWFgzYMkDkynqqwpqyNHrGdNjr@postgres.railway.internal:5432/railway?sslmode=require"
     echo: bool = True
 
