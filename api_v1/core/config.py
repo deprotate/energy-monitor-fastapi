db102fa071e1118dc12d57a1056d2e2eae17f33b
deprotate <gribozui.top@gmail.com>
2025-02-13 00:54:30 +0300

    prepare for deploy2

--- api_v1/core/config.py
+++ api_v1/core/config.py
@@ -5,21 +5,20 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    postgres_database: str
+    """postgres_database: str
     postgres_host: str
     postgres_password: str
     postgres_port: str
-    postgres_username: str
+    postgres_username: str"""
 
     @property
     def db_url(self):
-        return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database}"
+        return os.getenv("DATABASE_URL")
+       # return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database}"
 
     echo: bool = True
 
-    class Config:
-        env_file = ".env"
 
 
-dotenv.load_dotenv()
+#dotenv.load_dotenv()
 settings = Settings()
