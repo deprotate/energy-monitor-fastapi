6805c9577d4090229cc283b4818ffb30f97fd6eb
deprotate <gribozui.top@gmail.com>
2025-02-13 01:10:12 +0300

    prepare for deploy3.2

--- api_v1/core/config.py
+++ api_v1/core/config.py
@@ -11,13 +11,13 @@ class Settings(BaseSettings):
     postgres_port: str
     postgres_username: str"""
 
-    @property
+    """@property
     def db_url(self):
         url = os.getenv("DATABASE_URL")
         url = "postgresql+asyncpg"+ url[9::]
-        return url
+        return url"""
        # return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database}"
-
+    db_url: str = "postgresql+asyncpg://postgres:utehLuQWFgzYMkDkynqqwpqyNHrGdNjr@postgres.railway.internal:5432/railway"
     echo: bool = True
 
 
