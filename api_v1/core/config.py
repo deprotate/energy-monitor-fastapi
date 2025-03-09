17d199cc19bb70767992dd0dc0196a73ee1cfa20
deprotate <gribozui.top@gmail.com>
2025-03-09 20:32:53 +0300

    add predict_report

--- api_v1/core/config.py
+++ api_v1/core/config.py
@@ -1,28 +1,37 @@
 import os
 
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-
+    deploy: str = "local"
     host: str = "0.0.0.0"
     port: str = "8000"
 
-
     @property
     def db_url(self):
-        url = os.getenv("DATABASE_URL", "postgresql://postgres:bvLdusGmHleiyfhjsgGnPzMyTTGRzWll@postgres.railway.internal:5432/railway")
+        ### ____________________________
+        deploy: str = "local" # УЛЬТРАКОСТЫЛЬ НАДО ИСПРАВИТЬ
+        ### _____________________________
+        if deploy == "external":
+            url = os.getenv("DATABASE_URL",
+                            "postgresql://postgres:bvLdusGmHleiyfhjsgGnPzMyTTGRzWll@postgres.railway.internal:5432/railway")
 
-        if url.startswith("postgres://"):
-            url = url.replace("postgres://", "postgresql+asyncpg://", 1)
-        elif url.startswith("postgresql://"):
-            url = url.replace("postgresql://", "postgresql+asyncpg://", 1)
+            if url.startswith("postgres://"):
+                url = url.replace("postgres://", "postgresql+asyncpg://", 1)
+            elif url.startswith("postgresql://"):
+                url = url.replace("postgresql://", "postgresql+asyncpg://", 1)
 
-        return url
+            return url
+        else:
+            return "postgresql+asyncpg://postgres:admin@localhost:5432/energy_fastapi"
 
     # return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database}"
     echo: bool = False
+    solar_coefficient: float = 3.5
+    average_consumption: float = 500.0
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 
